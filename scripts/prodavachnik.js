182c28b769ea9df2f39834726e6f4c0adf86746d
Silviya Dimitrova <silviya_sd@yahoo.com>
2017-10-20 10:45:06 +0300

    task 4-1 Personalization added

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,4 +1,8 @@
 function startApp() {
+	if (sessionStorage.getItem('authToken') !== null) {
+        let username = sessionStorage.getItem('username');
+        $('#loggedInUser').text("Welcome, " + username + "!");
+    }
     showHideMenuLinks();
     showHomeView();
 
@@ -31,12 +35,14 @@ function startApp() {
             $("#linkRegister").show();
             $("#linkListAds").hide();
             $("#linkLogout").hide();
+            $("#loggedInUser").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
             $("#linkRegister").hide();
             $("#linkListAds").show();            
             $("#linkLogout").show();
+            $("#loggedInUser").show();
         }
     }
 
@@ -85,6 +91,10 @@ function startApp() {
         sessionStorage.setItem('authToken', userAuth);
         let userId = userInfo._id;
         sessionStorage.setItem('userId', userId);
+        let username = userInfo.username;
+        sessionStorage.setItem('username', username);
+        $('#loggedInUser').text("Welcome, " + username + "!");
+
     }
 
     // user/register
